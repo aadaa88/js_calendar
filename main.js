cb24185c126a7690232f9c92d801d5c4b449a870
4aDa4 <aadaa88@wedatalab.com>
2021-11-07 21:33:50 +0900

    created CreateCalendar Object

--- main.js
+++ main.js
@@ -14,32 +14,45 @@
 
 "use strict";
 
-const date = new Date();
-const currentYear = date.getFullYear();
-const currentMonth = date.getMonth();
-const currentDate = date.getDate();
-const currentDay = date.getDay();
+const currentDate = new Date();
 
+class CreateCalendar {
+    constructor(currentDate = Date()) {
+        this.currentDate = currentDate;
+        this.calendarFrame = this.createFrame(this.currentDate);
+    }
 
-function myCalendar() {
-    //   입력으로 Data() 받으며 날짜 정보 받을 수 있음
-    // 1. 날짜정보를 통해 해당 년, 월, 요일 정보 추출
-    // 2. 해당 월이 몇 일날이 있는지 추출
-    // 3. 한달 전체를 표현 할 수 있는 테이블 생성 6x7(42cell)
-    // 4. 날짜 데이터 프레임 생성
-    // 5. 테이블을 채우기 
-    // 6. 화면 출력
+    createFrame() {
+        const _currentYear = this.currentDate.getFullYear();
+        const _currentMonth = this.currentDate.getMonth();
+        this.calendarFrame = {};
 
-    const main = document.querySelector('.main');
+        const _firstDay = new Date(_currentYear, _currentMonth, 1).getDay(); // 해당 월의 첫번째 요일을 추출
 
-    const frameOfCalendar = createFrame(date);
-    const calTable = createTable(frameOfCalendar);
-    main.appendChild(calTable);
+        for (let i = 0; i < 42; i++) {
+            this.calendarFrame[`${i}`] = new Date(_currentYear, _currentMonth, 1 - _firstDay + i).getDate();
+        }
+        return this.calendarFrame;
+    }
 
+    addTask() {
+
+    }
+    getTask() {
+
+    }
+    updateTask() {
+
+    }
+    deleteTask() {
+
+    }
 }
-function createFrame(date) {
-    const _currentYear = date.getFullYear();
-    const _currentMonth = date.getMonth();
+console.log(new CreateCalendar(currentDate))
+
+function createFrameOld(currentDate) {
+    const _currentYear = currentDate.getFullYear();
+    const _currentMonth = currentDate.getMonth();
 
     let _frameOfCalendar = [];
     const _firstDay = new Date(_currentYear, _currentMonth, 1).getDay(); // 해당 월의 첫번째 요일을 추출
@@ -50,7 +63,26 @@ function createFrame(date) {
     return _frameOfCalendar;
 }
 
-function createTable(frameOfCalendar) {
+function myCalendar() {
+    //   입력으로 Data() 받으며 날짜 정보 받을 수 있음
+    // 1. 날짜정보를 통해 해당 년, 월, 요일 정보 추출
+    // 2. 해당 월이 몇 일날이 있는지 추출
+    // 3. 한달 전체를 표현 할 수 있는 테이블 생성 6x7(42cell)
+    // 4. 날짜 데이터 프레임 생성
+    // 5. 테이블을 채우기 
+    // 6. 화면 출력
+    // 7. encapsulation
+
+
+    const main = document.querySelector('.main');
+
+    const frameOfCalendar = createFrameOld(currentDate);
+    const calTable = createTable(currentDate, frameOfCalendar, 'ja-JP');
+    main.appendChild(calTable);
+
+}
+
+function createTable(currentDate, frameOfCalendar, locale = 'ko-KR') {
     const calTable = document.createElement('table');
     calTable.style.width = '100%';
     calTable.setAttribute('border', '1');
@@ -60,12 +92,12 @@ function createTable(frameOfCalendar) {
 
     for (let h = 0; h < 7; h++) {
         const headCell = document.createElement('th');
-        const headText = document.createTextNode(new Date(currentYear, currentMonth, h).toLocaleDateString('ko', { weekday: 'long' }));
+        const headText = document.createTextNode(new Date(0, 0, h).toLocaleDateString(locale, { weekday: 'long' }));
         headCell.appendChild(headText);
         thead.appendChild(headCell);
     }
 
-    let sum = 0;
+    let idx = 0;
     for (let i = 0; i < 6; i++) {
         const row = calTableBody.insertRow(); // document객채가 공식적으로 html table 택을 다루는 기능을 지원해 주고 있으로 이것을 사용하는 것을 선호합니다.
 
@@ -73,9 +105,9 @@ function createTable(frameOfCalendar) {
         for (let j = 0; j < 7; j++) {
             let cell = row.insertCell();
             // var cell = document.createElement('td');
-            // let cellText = document.createTextNode('{cell ' + sum + '} col: ' + j + ', row: ' + i);
-            let cellText = document.createTextNode(`${frameOfCalendar[sum]}`);
-            sum++;
+            // let cellText = document.createTextNode('{cell ' + idx + '} col: ' + j + ', row: ' + i);
+            let cellText = document.createTextNode(`${frameOfCalendar[idx]}`);
+            idx++;
             cell.appendChild(cellText);
             // row.appendChild(cell); // insertCell()를 사용하게 되면 이런씩으로 코드 줄 주릴 수 있음
         }
@@ -85,7 +117,7 @@ function createTable(frameOfCalendar) {
     let title = document.createElement('caption');
     title.appendChild(document.createTextNode('Generating The Calendar using <table> tag in javascript!'));
     title.appendChild(document.createElement('br'));
-    title.appendChild(document.createTextNode(date));
+    title.appendChild(document.createTextNode(currentDate));
 
     calTable.appendChild(title);
     calTable.appendChild(thead);
@@ -152,10 +184,10 @@ var dString = "May, 20, 1984";
 var d1 = new Date(dString);
 var d2 = new Date();
 
-console.log("Number of <b>days</b> since " + dString + ": " + DateDiff.inDays(d1, d2));
-console.log("Number of <b>weeks</b> since " + dString + ": " + DateDiff.inWeeks(d1, d2));
-console.log("Number of <b>months</b> since " + dString + ": " + DateDiff.inMonths(d1, d2));
-console.log("Number of <b>years</b> since " + dString + ": " + DateDiff.inYears(d1, d2));
+// console.log("Number of <b>days</b> since " + dString + ": " + DateDiff.inDays(d1, d2));
+// console.log("Number of <b>weeks</b> since " + dString + ": " + DateDiff.inWeeks(d1, d2));
+// // console.log("Number of <b>months</b> since " + dString + ": " + DateDiff.inMonths(d1, d2));
+// console.log("Number of <b>years</b> since " + dString + ": " + DateDiff.inYears(d1, d2));
 
 /* Scoping rules
 The main difference is scoping rules. Variables declared by var keyword are scoped to the immediate function body (hence the function scope) while let variables are scoped to the immediate enclosing block denoted by { } (hence the block scope).
